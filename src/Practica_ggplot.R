9e3d1d4acc5cedd208a0049988b52104079f5e44
Ana Maritza Bello <amby.121192@gmail.com>
2024-08-06 20:15:47 -0600

    🎉 Add examples of Rmd files with examples of datacamp courses

--- src/Practica_ggplot.R
+++ src/Practica_ggplot.R
@@ -7,7 +7,12 @@ items=c("potato", "rice", "oil")
 
 all_prices=data.frame(items, jan_price, mar_price, june_price)
 
-ggplot(all_prices, aes(x=items, y=jan_price)) + geom_point() + geom_point(stat="summary", fun=mean, colour="red", size=3)
+ggplot(all_prices, aes(x=items, y=jan_price)) + 
+    geom_point() + 
+    geom_point(stat="summary", fun=mean, colour="red", size=3)
 
-ggplot(all_prices, aes(x = jan_price, y = june_price)) + geom_point() + facet_grid(. ~ items) + stat_smooth(method = "lm", se = TRUE, col='blue')
+ggplot(all_prices, aes(x = jan_price, y = june_price)) + 
+    geom_point() + 
+    facet_grid(. ~ items) + 
+    stat_smooth(method = "lm", se = TRUE, col='blue')
 
